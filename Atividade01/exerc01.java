f6db65698c7f09208e1d639116567a59eda3a6ff
Fábio dos Santos Silva <88898316+FabioSilva76@users.noreply.github.com>
2023-02-10 23:11:00 -0300

    Update exerc01.java

--- Atividade01/exerc01.java
+++ Atividade01/exerc01.java
@@ -3,6 +3,7 @@ package Atividade01;
 public class exerc01 {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
+        System.out.println("oi gente!!!");
     }
     
 }
